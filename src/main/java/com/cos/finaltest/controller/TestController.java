39c46ec19035c3e089942005bdeb4fbc27c6114a
jg20228 <jg20228@nate.com>
2020-09-09 10:35:22 +0900

    init

---
+++ src/main/java/com/cos/finaltest/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.cos.finaltest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping({"","/"})
+	public String test() {
+		return "index";
+	}
+	
+	@GetMapping("/stardium")
+	public String stardiumTest() {
+		return "stardium/stardium";
+	}
+	
+	@GetMapping("/stardiumSaveForm")
+	public String stardiumSaveFormTest() {
+		return "stardium/stardiumSaveForm";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/stardium/stardiumSaveForm.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@include file="../layout/header.jsp"%>
+<div class="container">
+<h1>경기장 등록 페이지입니다.</h1>
+</div>
+<%@include file="../layout/footer.jsp"%>
